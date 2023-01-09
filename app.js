d119e85ceaa420f001190e84095bcb8709ea0035
Rakesh Mehra <66615486+Rakeshmehra123@users.noreply.github.com>
2023-01-09 22:57:49 +0530

    text input updated

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var btnTranslate=document.querySelector("#btn-translate");
-
-
-document.addEventListener("click", function clickHandler("click", btnTranslate){
+var textInput=document.querySelector("#txt-input")
+function clickHandler(){
     console.log("clicked!");
-})
+    console.log("Input", textInput.value)
+}
+
+
+btnTranslate.addEventListener("click", clickHandler)
